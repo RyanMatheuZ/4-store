cd40ea5cbee74b25f8333dc258d99c42bbe7a57d
Ryan Oliveira <oryan424@gmail.com>
2022-02-03 23:36:42 -0300

    Developed forms

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
-import './login.css'
+import '../../forms.css'
 
 export default function Login() {
 	return (
@@ -9,7 +11,61 @@ export default function Login() {
 			<Header />
 
 			<main>
-				<h1>Login</h1>
+				<div className="container">
+					<header className="header-form">
+						<h1 className="header-form__title">
+							Olá novamente!
+						</h1>
+
+						<p className="header-form__text">
+							Entre em sua conta
+						</p>
+					</header>
+
+					<form className="form">
+						<div className="form__box">
+							<label htmlFor="email" className="form__label">
+								E-mail:
+							</label>
+
+							<input
+								type="text"
+								id="email"
+								placeholder="nome@endereco.com"
+								className="form__input"
+							/>
+						</div>
+
+						<div className="form__box">
+							<label htmlFor="password" className="form__label">
+								Senha:
+							</label>
+
+							<input
+								type="password"
+								id="password"
+								placeholder="••••••••••"
+								className="form__input"
+							/>
+						</div>
+
+						<div className="form__box">
+							<button className="form__button">
+								Entrar
+							</button>
+						</div>
+					</form>
+
+					<div className="warning">
+						<p className="warning__text">
+							Não possui uma conta ainda?
+						</p>
+
+						<Link to="/register" className="warning__link">
+							Criar uma agora
+						</Link>
+					</div>
+				</div>
 			</main>
 
 			<Footer />
--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -1,7 +1,7 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
-import './register.css'
+import '../../forms.css'
 
 export default function Register() {
 	return (
@@ -9,7 +9,83 @@ export default function Register() {
 			<Header />
 
 			<main>
-				<h1>Register</h1>
+				<div className="container">
+					<header className="header-form">
+						<h1 className="header-form__title">
+							Registre-se!
+						</h1>
+
+						<p className="header-form__text">
+							Crie sua nova conta
+						</p>
+					</header>
+
+					<form className="form">
+						<div className="form__box">
+							<label htmlFor="name" className="form__label">
+								Nome:
+							</label>
+
+							<input
+								type="text"
+								id="name"
+								placeholder="João"
+								className="form__input"
+							/>
+						</div>
+
+						<div className="form__box">
+							<label htmlFor="person-id" className="form__label">
+								CPF:
+							</label>
+
+							<input
+								type="number"
+								id="person-id"
+								placeholder="xxx.xxx.xxx-xx"
+								className="form__input"
+							/>
+						</div>
+
+						<div className="form__box">
+							<label htmlFor="email" className="form__label">
+								E-mail:
+							</label>
+
+							<input
+								type="text"
+								id="email"
+								placeholder="nome@endereco.com"
+								className="form__input"
+							/>
+						</div>
+
+						<div className="form__box">
+							<label htmlFor="password" className="form__label">
+								Senha:
+							</label>
+
+							<input
+								type="password"
+								id="password"
+								placeholder="••••••••••"
+								className="form__input"
+							/>
+						</div>
+
+						<div className="form__box">
+							<button className="form__button">
+								Registrar
+							</button>
+						</div>
+					</form>
+
+					<div className="warning">
+						<p className="warning__text">
+							<span className="warning__span">Ao registrar-se, você concorda com nossos</span> termos de <br /> uso <span className="warning__span">e</span> política de privacidade
+						</p>
+					</div>
+				</div>
 			</main>
 
 			<Footer />
