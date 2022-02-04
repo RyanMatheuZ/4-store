5feb57cb0ba60966ecb665d2543da9bc77b46158
Ryan Oliveira <oryan424@gmail.com>
2022-02-04 15:23:34 -0300

    Developed product page

--- src/views/Products/index.jsx
+++ src/views/Products/index.jsx
@@ -1,15 +1,63 @@
+import { useEffect, useState } from 'react'
+
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
 import './products.css'
 
 export default function Products() {
+	const [products, setProducts] = useState([])
+
+	async function getProducts() {
+		const response = await fetch('https://fakestoreapi.com/products')
+		const data = await response.json()
+
+		setProducts(data)
+
+		console.log(data)
+	}
+
+	useEffect(() => {
+		getProducts()
+	}, [])
+
 	return (
 		<>
 			<Header />
 
 			<main>
-				<h1>Products</h1>
+				<section className="container product-box">
+					{products && (
+						products.map(item => (
+							<article key={ item.id } className="product-box__item">
+								<figure className="product-box__image">
+									<img 
+										src={ item.image } 
+										alt={ item.title } 
+										title={ item.title } 
+										draggable="false"
+										width={ 100 }
+									/>
+								</figure>
+
+								<span className="product-box__name" title="Nome">
+									{ item.title }
+								</span>
+
+								<div className="product-box__info">
+									<span className="product-box__price" title="Preço">
+										<span className="product-box__price-sign">R$</span> 
+										{ item.price }
+									</span>
+
+									<button className="product-box__button" title="Comprar produto">
+										Comprar
+									</button>
+								</div>
+							</article>
+						))
+					)}
+				</section>
 			</main>
 
 			<Footer />
